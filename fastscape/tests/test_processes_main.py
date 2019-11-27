756d5f84c960e8ee090736f2a4523959fe51a308
Benoit Bovy <benbovy@gmail.com>
2019-11-27 12:30:56 +0100

    wip add more tests main processes

--- fastscape/tests/test_processes_main.py
+++ fastscape/tests/test_processes_main.py
@@ -4,7 +4,8 @@ import pytest
 from fastscape.processes import (Bedrock,
                                  SurfaceToErode,
                                  SurfaceTopography,
-                                 TotalVerticalMotion)
+                                 TotalVerticalMotion,
+                                 UniformSedimentLayer)
 
 
 def test_total_vertical_motion():
@@ -47,34 +48,60 @@ def test_surface_to_erode():
 
     p = SurfaceToErode(topo_elevation=elevation)
 
-    p.initialize()
-
-    np.testing.assert_equal(p.elevation, elevation)
-
-    p.topo_elevation += 1
-
     p.run_step()
 
     np.testing.assert_equal(p.elevation, p.topo_elevation)
 
 
-@pytest.mark.parametrize('surface_up,bedrock_up,surf_delta,outcome', [
-    (1, 0, 1, 'all_lower'),
+def test_bedrock_error():
+    elevation = np.ones((3, 2))
+    surface_elevation = np.zeros_like(elevation)
 
-])
-def test_berock(surface_up, bedrock_up, surf_delta, outcome):
-    grid_shape = (3, 2)
-    elevation = np.random.uniform(size=grid_shape)
-    surf_elevation = elevation + surf_delta
+    with pytest.raises(ValueError, match=r".* bedrock elevation higher .*"):
+        p = Bedrock(elevation=elevation,
+                    surface_elevation=surface_elevation,
+                    bedrock_motion_up=np.zeros_like(elevation),
+                    surface_motion_up=np.zeros_like(elevation))
+
+        p.initialize()
+
+
+def test_berock():
+    elevation = np.array([[0., 0., 0.],
+                          [2., 2., 2.]])
+    surface_elevation = np.array([[1., 1., 1.],
+                                  [2., 2., 2.]])
 
     p = Bedrock(elevation=elevation,
-                bedrock_motion_up=np.ones_like(elevation) * bedrock_up,
-                surface_motion_up=np.ones_like(elevation) * surface_up,
-                surface_elevation=surf_elevation)
+                bedrock_motion_up=np.full_like(elevation, 0.5),
+                surface_motion_up=np.full_like(elevation, -0.1),
+                surface_elevation=surface_elevation)
 
     p.initialize()
     p.run_step()
+
+    expected = np.array([[1., 1., 1.],
+                         [0., 0., 0.]])
+    np.testing.assert_equal(p._depth(), expected)
+
     p.finalize_step()
 
-    if outcome == 'all_lower':
-        assert np.all([True])
+    expected = np.array([[0.5, 0.5, 0.5],
+                         [1.9, 1.9, 1.9]])
+    np.testing.assert_equal(p.elevation, expected)
+
+
+def test_uniform_sediment_layer():
+    grid_shape = (3, 2)
+    bedrock_elevation = np.random.uniform(size=grid_shape)
+    surf_elevation = np.random.uniform(size=grid_shape) + 1
+    expected = surf_elevation - bedrock_elevation
+
+    p = UniformSedimentLayer(bedrock_elevation=bedrock_elevation,
+                             surf_elevation=surf_elevation)
+
+    p.initialize()
+    np.testing.assert_equal(p.thickness, expected)
+
+    p.run_step()
+    np.testing.assert_equal(p.thickness, expected)
